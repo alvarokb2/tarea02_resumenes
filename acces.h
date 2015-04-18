ea61f8d09f419965bfe31cc71399231d5c6fe3ed
alvarokb2 <pastalvaro@gmail.com>
2015-04-18 00:30:18 -0300

    Create acces.h

---
+++ acces.h
@@ -0,0 +1,44 @@
+/* 
+ * File:   access.h
+ * Author: alvarokb2
+ *
+ * Created on 16 de abril de 2015, 12:14
+ */
+
+#ifndef ACCESS_H
+#define	ACCESS_H
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+#include "access.h"
+#include "Peticion.cpp"
+
+void leerLinea() {
+    ifstream flujo("access.log", ios::in);
+    string linea;
+    int count = 0;
+    if (flujo.fail()) {
+        cout << "Error al abrir archivo" << endl;
+    } else {
+        while (!flujo.eof()) {
+            getline(flujo, linea);
+            Peticion *actual = new Peticion(linea);
+
+            cout << "ip: " << actual->getIp() << "/ fecha: " << actual->getFecha() << "/ estado: " << actual->getEstado() << endl;
+            cout << "count: " << count << "/ so: " << actual->getSo() << endl;
+            count++;
+        }
+        flujo.close();
+    }
+}
+
+
+#endif	/* ACCESS_H */
+
