ddc9fbe0a0917103487dbbcbda16a238e65ba520
alvarokb2 <pastalvaro@gmail.com>
2015-04-19 17:41:12 -0300

    Update leer.h
    fix, las funciones ya no devuelven null

--- leer.h
+++ leer.h
@@ -13,59 +13,42 @@ using namespace std;
 
 booTokenPtr leerAccess(char * fecha_i, char * fecha_f, char * archivo) {
     FILE * files = fopen(archivo, "r");
-    booTokenPtr d;
+    booTokenPtr response = NULL;
     if (files != NULL) {
-        d = getTokensAccess(files,fecha_i, fecha_f, -1);
-        if (d != NULL) {
-            int i = 0;
-            do {
-                imprimir(d);
-                d = d->next;
-            } while (d != NULL);
-        }
+        response = getTokensAccess(files, fecha_i, fecha_f, -1);
     } else {
         cout << "No se puede abrir el archivo." << endl;
     }
     fclose(files);
-    return d;
+    return response;
 }
 
 booTokenPtr leerCsvLocations() {
     FILE * files = fopen("GeoLite2-City-Locations-es.csv", "r");
-    booTokenPtr d;
+    booTokenPtr response = NULL;
     if (files != NULL) {
-        d = getTokensLocations(files, -1);
-        if (d != NULL) {
-            int i = 0;
-            do {
-                d = d->next;
-            } while (d != NULL);
-        }
+        response = getTokensLocations(files, -1);
+        
     } else {
         cout << "No se puede abrir el archivo." << endl;
     }
     fclose(files);
-    return d;
+    return response;
 }
 
 booTokenPtr leerCsvBlock() {
     FILE * files = fopen("GeoLite2-City-Blocks-IPv4.csv", "r");
-    booTokenPtr d;
+    booTokenPtr response = NULL;
     if (files != NULL) {
-        d = getTokensBlock(files, -1);
-        if (d != NULL) {
-            int i = 0;
-            do {
-                d = d->next;
-            } while (d != NULL);
-
-        }
+        response = getTokensLocations(files, -1);
+        
     } else {
         cout << "No se puede abrir el archivo." << endl;
     }
     fclose(files);
-    return d;
+    return response;
 }
+
 
 
 
